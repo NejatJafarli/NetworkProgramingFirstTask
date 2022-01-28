634907ce3db886ae901524bea474ef2fe6ff94f2
nicat ceferli <mega.ceferli@gmail.com>
2022-01-28 17:55:28 +0400

    Add project files.

---
+++ NetworkProgramingFirstTask/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkProgramingFirstTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public Stream MyStream { get; set; }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var Client = new TcpClient();
+            Client.Connect(IPAddress.Loopback.ToString(), 1111);
+
+            MyStream = Client.GetStream();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                byte[] buffer = new byte[1000000];
+
+                MyStream.Read(buffer, 0, buffer.Length);
+
+
+                var image = new BitmapImage();
+                using (var mem = new MemoryStream(buffer))
+                {
+                    mem.Position = 0;
+                    image.BeginInit();
+                    image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.UriSource = null;
+                    image.StreamSource = mem;
+                    image.EndInit();
+                }
+                image.Freeze();
+                Img.Source = image;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            //byte[] buffer = new byte[ulong.MaxValue];
+
+            //MyStream.Read(buffer, 0, buffer.Length);
+
+            //var MyImage = new BitmapImage();
+
+            //System.Drawing.Image a = null;
+            //Bitmap b = null;
+            //using (var ms = new MemoryStream(buffer))
+            //{
+
+            //    a = System.Drawing.Image.FromStream(ms);
+            //    b = new Bitmap(a);
+
+            //}
+            //b.Save("Client.jpg");
+
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.BeginInit();
+            //bitmap.UriSource = new Uri("Client.jpg");
+            //bitmap.EndInit();
+            //Img.Source = bitmap;
+
+
+        }
+    }
+}
---
+++ Tcp_Server/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var ip = IPAddress.Loopback;
+        var port = 1111;
+        var TcpServer = new TcpListener(ip, port);
+
+        Console.WriteLine("Lisening ", ip);
+
+        TcpServer.Start(100);
+        List<TcpClient> clients = new List<TcpClient>();
+        Task.Run(() =>
+        {
+            while (true)
+            {
+                var client = TcpServer.AcceptTcpClient();
+                clients.Add(client);
+                Console.WriteLine($"{client.Client.RemoteEndPoint} Client Connected");
+            }
+        });
+        while (true)
+        {
+            while (true)
+            {
+                var ServerText = Console.ReadLine();
+                if (ServerText == "Send")
+                    break;
+            }
+            using var bitmap = new Bitmap(1920, 1080);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.CopyFromScreen(0, 0, 0, 0,
+                    bitmap.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            bitmap.Save("SSA.jpg", ImageFormat.Jpeg);
+
+            Image img = (Image)bitmap;
+
+            byte[] bytes = (byte[])(new ImageConverter()).ConvertTo(img, typeof(byte[]));
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Connected)
+                {
+                    BinaryWriter binaryWriter = new BinaryWriter(clients[i].GetStream());
+                    binaryWriter.Write(bytes.ToArray());
+                }
+            }
+            //convert byte array to image
+            //Image myimg;
+            //using (var ms = new MemoryStream(bytes))
+            //{
+            //    myimg=Image.FromStream(ms);
+            //}
+
+
+            //myimg.Save("Hello.jpg", ImageFormat.Jpeg);
+
+        }
+
+    }
+}
